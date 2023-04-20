e408fedd4b0ac1d39b724ccd48d2465142de14a0
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-04-20 13:55:27 +0530

    Update
    Added persistor for state preservation

--- store/store.ts
+++ store/store.ts
@@ -1,9 +1,21 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storageSession from 'redux-persist/lib/storage/session'
 import thunk from "redux-thunk";
 import rootReducer from "./reducer";
 
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     {},
     applyMiddleware(thunk)
 )
+
+export const persistor = persistStore(store)
+
