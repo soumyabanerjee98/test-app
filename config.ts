0b8da3c9f391015b007c55091ee5d31badf509c5
Soumya Banerjee <banerjeesoumya771@gmail.com>
2023-04-20 21:01:47 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const server = {
     test: 'http://localhost:3000/api/',
-    live: 'https://currency-and-weather-app.vercel.app/',
+    live: 'https://currency-and-weather-app.vercel.app/api/',
     forecastDay: 7
 }
 export const processIDs = {
